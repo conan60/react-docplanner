a408199e211ef76d9a4521bab63b3b4d80b6f8f5
conan60 <malek.gorchene@gmail.com>
2019-08-06 13:58:07 +0100

    Updates

---
+++ precache-manifest.b30b23825d85165a806afaa7271e88ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37f26b209655bf7b19849f28b9c62f5",
+    "url": "/react-docplanner/index.html"
+  },
+  {
+    "revision": "d4ba5c28e3822b1b06b6",
+    "url": "/react-docplanner/static/css/main.baa2cf18.chunk.css"
+  },
+  {
+    "revision": "b7a60f0615fcc2c3bea5",
+    "url": "/react-docplanner/static/js/2.f4232aad.chunk.js"
+  },
+  {
+    "revision": "d4ba5c28e3822b1b06b6",
+    "url": "/react-docplanner/static/js/main.edf59349.chunk.js"
+  },
+  {
+    "revision": "b983491042526d2d47f1",
+    "url": "/react-docplanner/static/js/runtime~main.97f299d1.js"
+  }
+]);
